0d373e0879b0d4f8f53c7c28c8b4303af7c47feb
James Grom <jamesgrom@Jamess-MacBook-Pro.local>
2020-12-09 14:25:30 -0800

    started server listening to port 3000 localhost

--- server.js
+++ server.js
@@ -4,3 +4,22 @@ console.log("server.js started running");
 const express = require('express');
 const fs = require('fs/promises');
 const path = require('path');
+
+//main function
+const main = () => {
+    const app = express();
+    const port = 3000;
+    app.use(express.json());
+    
+    //define the root path
+    app.get('/', (req,res) =>{
+        res.sendFile('./htmlFiles/index.html',{root: path.join(__dirname,'')})
+    });
+
+    app.listen(port,() => {
+        console.log(`queueManager server started on http://localhost: ${port}`);
+    });
+
+}
+
+main();
