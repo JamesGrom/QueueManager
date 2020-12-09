c6d07b156e92c86234e983f857b6ee2e874e5ba5
James Grom <jamesgrom@Jamess-MacBook-Pro.local>
2020-12-09 14:15:34 -0800

    Updated the package.json to include the express dependency

---
+++ server.js
@@ -0,0 +1,6 @@
+console.log("server.js started running");
+
+//require the needed express functionality
+const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
