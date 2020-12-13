2357fb31713cb54f32c6f664580a984a69e5b5a1
unknown <sdelacruz@scu.edu>
2020-12-12 19:04:36 -0700

    implementation of database included

--- static/javascriptFiles/HelpfulResources.js
+++ static/javascriptFiles/HelpfulResources.js
@@ -1,90 +1,67 @@
-//this will contain the code necessary for having the button
-//idea is that whent eh button is pressed
-//a textbox will open right underneat having
-//a description tag and a link tag
-//then the code will run and will make an anchor
-//with the description as the text, likn as..link
-
-
 //ez directory line cd '.\TempCoenFolder\C161\C161 Project\GITHUB\QueueManager\'
 
-//<input type='text' value='Enter some text here'>
-//https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test
-//^ also good
 const ResourceButton = document.getElementById('ResourceButton');
-console.log(ResourceButton);
 
+function ErrorText(){ //is only called when the user submits a faulty submit
+    if(document.getElementById('ErrorText')) //double checks if there is already an error text on display
+        return;
 
-function handleResourceButtonClicked(){
-    //console.log("Event Handle click thingy");
+    const TopSection = document.getElementById('TopSection');
+    let Error = document.createElement('p');
+    Error.setAttribute("id", "ErrorText");
+    Error.innerText = 'Both fields must be filled'
+    TopSection.appendChild(Error);
+    return;
+}
+
+function addResourceLink(ResLInk, ResDescrip){ //focuses solely on adding a resource link into the page
+    //remove the error text if its there
+    if(document.getElementById('ErrorText')){ //if error text is already present, remove it
+        document.getElementById('ErrorText').remove();
+    }
+
+    const List = document.getElementById('ResourceLinks'); //the unordered list
+    let ResourceDescriptionNode = document.createElement('li'); //new list item
+    let ResourceLink = document.createElement('a');
+
+    ResourceLink.setAttribute('href', ResLink); //<a href=ResLink> </a>
+    ResourceLink.innerText = ResDescrip; //we want the text inside the anchor, <a> innerText </a> 
+
+    ResourceDescriptionNode.appendChild(ResourceLink); //<li> <a ResLink> ResDescrip </a></li>
+    
+    List.appendChild(ResourceDescriptionNode); // make it part of the unordered list
+    
+}
+
+function handleResourceButtonClicked(){ //either make a new Resource Link or produces an error text
     const ResDescrip = document.getElementById('ResDescrip').value;
     const ResLink = document.getElementById('ResLink').value;   
-    //add an input box
-    /*console.log("ResLink below")
-    console.log(ResLink);
-    console.log("ResDescrip below")
-    console.log(ResDescrip);
-    */
 
-   /* let ResourceObject = {
+    if(!ResLink || !ResDescrip){ //if either of the textboxes are blank, showcase the error text and do nothing else
+        ErrorText();
+        return;
+    }
+
+   let ResourceObject = { //upload this object into the Resource Link database
         link: ResLink,
         description: ResDescrip,
-        author: UserInfo.name
     }
-    */
-
-    /*fetch("http://..../createLink", {
+    fetch("/api/resources", {
         method: "POST",
-        body: JSON.stringify(temp)
-    //})
-    //.then( */
-        if(!ResLink || !ResDescrip){
-            //add a little red text thing below button
-            //both fields must be valid
-            if(document.getElementById('ErrorText'))
-                return;
-    
-            const TopSection = document.getElementById('TopSection');
-            let Error = document.createElement('p');
-            Error.setAttribute("id", "ErrorText");
-            Error.innerText = 'Both fields must be filled'
-            TopSection.appendChild(Error);
-            return;
-        }
-    
-        //remove the error text if its there
-        if(document.getElementById('ErrorText')){
-            document.getElementById('ErrorText').remove();
-        }
-        //create another bulletpoint in the body
-        //the inner text is the descriptoini
-        //the anchor is the link
-        const List = document.getElementById('ResourceLinks');
-        let ResourceDescriptionNode = document.createElement('li');
-        let ResourceLink = document.createElement('a');
-        
-        //ResourceLink.href = ResLink; //CHECK THIS ONE OUT I DONT KNOW
-        ResourceLink.setAttribute('href', ResLink);
-        ResourceLink.innerText = ResDescrip;
-    
-        //ResourceDescription.appendChild(ResourceLink); // <li> <a>
-    
-        ResourceDescriptionNode.appendChild(ResourceLink);
-        
-        List.appendChild(ResourceDescriptionNode);
-        
-        
-        //console.log(List);
-    //);
+        body: JSON.stringify(ResourceObject)
+    })
+    .then(addResourceLink(ResLink, ResDescrip)); //convert the text into a link 
 }
+
+window.onload = () => { //go through any and all resource links already uploaded in the past, display them using addResourceLink
+    fetch("http://localhost:3000/api/Resources")
+    .then((response) => (response.ok ? response.json() : Promise.reject()))
+    .then((data) => {
+        console.log(data);
+    })
+};
 
 function main() {
-    ResourceButton.addEventListener('click', () => { handleResourceButtonClicked() }); //we do this syntax because we don't want the stack function to use the event, we want it to take in specific arguments
+    ResourceButton.addEventListener('click', () => { handleResourceButtonClicked() });
 }
 main()
-//test comment
-/*
- git status
- git add filename
- git push
- */
